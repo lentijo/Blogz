2c70e5185edac9834a7da615943f008e494419a2
Luis Lentijo <llent.prog@gmail.com>
2018-10-07 19:30:49 -0500

    added code for SQL DB(not completed) added blog.html and new_post.html

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:chloe@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column.String(120)
+
+    def __init__(self, name):
+        self.name = name
 
 tasks = []
 
@@ -14,5 +26,5 @@ def index():
 
     return render_template('blog.html',title="Build A Blog", tasks=tasks)
 
-
-app.run()
+if __name__ = "__main__":
+    app.run()
