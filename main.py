ad16bea00ed5cfc03f86d173adcb6c5762ee8320
Luis Lentijo <llent.prog@gmail.com>
2018-10-21 15:43:08 -0500

    this was done last class 10-14, created single_post.html to display blog entries individually, fixed base.html to display errors, fixed blog.html to display blogs with link capabilities

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:chloe@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 class Blog(db.Model):
     
@@ -23,29 +24,44 @@ class Blog(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    blogs = Blog.query.all()
+    return render_template('blog.html',title="Build A Blog", blogs=blogs)
     
-    
-
-
-    return render_template('blog.html')
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
-    blogs = Blog.query.all()
-    return render_template('blog.html',title="Build A Blog", blogs=blogs)
+    blogs = Blog.query.all() 
+    return render_template('blog.html', blogs=blogs)
 
 @app.route('/new_post', methods=['POST', 'GET'])
 def new_post():
+    blogs = Blog.query.all()
+ 
+    
     if request.method == 'POST':
         title_name = request.form['title']
         body_name = request.form['body']
         new_blog= Blog(title_name, body_name)
-        db.session.add(new_blog)
-        db.session.commit()
+ 
+        
+        if not title_name or not body_name:
+            flash('All fields must be filled! Please try again.','error')
+            return render_template('new_post.html', title_name=title_name, body_name=body_name)
+        
+        else:
+            db.session.add(new_blog)
+            db.session.commit()
+            blog_id=new_blog.id
+            return redirect('/single_post?id={0}' .format(blog_id))
       
-    return render_template('new_post.html')
-
     
+    return render_template('new_post.html', blogs=blogs)
+
+@app.route('/single_post')
+def view_post():
+    id = request.args.get('id')
+    blog_query = Blog.query.filter_by(id=id).all()
+    return render_template('single_post.html', title="", blog_query=blog_query)
 
 if __name__ == "__main__":
     app.run()
