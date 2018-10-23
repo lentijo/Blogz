15a8ebd63c9cdd3ca25b2e89290b7cb8e050663c
Luis Lentijo <llent.prog@gmail.com>
2018-10-22 19:54:50 -0500

    changed main.py fixed register variable, base.html added links for login, logout, current main.py allows to register, login and add blog. dropped iniial db and created new db with user/pass

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -20,7 +20,68 @@ class Blog(db.Model):
         self.body = body
 
 
+class User(db.Model):
 
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email'] = email
+            flash("Logged in")
+            return redirect('/')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+
+    return render_template('login.html')
+
+
+
+@app.route('/signup' , methods=['POST' , 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+    #TODO - validate user's data
+       
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/')
+            #TODO - "remember" the user
+        else:
+            #TODO - user better response messaging
+            return '<h1>Duplicate user</h1>'
+
+
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
